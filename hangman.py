420384911fb693b87025331ccd31547041d0eb93
dariszka <darinapetrova05@gmail.com>
2022-10-27 12:50:20 +0200

    Add way of inputting and assessing whole word guesses.

--- hangman.py
+++ hangman.py
@@ -34,8 +34,13 @@ def make_guess():
         if len(current_guess) == 1:
             return current_guess
         else:
-            print('Just one letter b.')
-            return make_guess()
+            full_word_confirmation = input('Do you want to guess the whole word?\n')
+            possible_confirmations = ['y', 'yes', 'yeah', 'sure']
+            if full_word_confirmation.lower() in possible_confirmations:
+                return current_guess
+            else:
+                print('Yeah thats what I thought')
+                return current_guess
     else:
         print('Babez do you know what a letter is? Try again..')
         return make_guess()
@@ -43,29 +48,42 @@ def make_guess():
 def play_game(word_letters_array):
     guessed_letters = []
     wrong_guessed_letters = '' 
-    left_tries = 3
+    left_tries = 5
     while left_tries > 0:
         current_guess = make_guess()
-        if current_guess in guessed_letters:
-            print('You already guessed that girlie.')
-        elif current_guess in wrong_guessed_letters:
-            print('You literally already tried that.')
-        else: 
-            guessed_letters += current_guess
-            if current_guess in word_letters_array:
-                current_guess_progress = display_word(guessed_letters, word_letters_array)
-                if current_guess_progress == ''.join(word_letters_array):
-                    print('OMG SLAY U WIN')
-                    break
+        if len(current_guess) == 1:
+            if current_guess in guessed_letters:
+                print('You already guessed that girlie.')
+            elif current_guess in wrong_guessed_letters:
+                print('You literally already tried that.')
+            else: 
+                guessed_letters += current_guess
+                if current_guess in word_letters_array:
+                    current_guess_progress = display_word(guessed_letters, word_letters_array)
+                    if current_guess_progress == ''.join(word_letters_array):
+                        print('OMG SLAY U WIN')
+                        break
+                else:
+                    left_tries -=1 
+                    if left_tries == 0:
+                        print('Oh noo, looks like ur a lil looser baby. U can always try again doe.')
+                        break
+                    else: 
+                        guessed_letters.remove(current_guess)
+                        wrong_guessed_letters += current_guess + ' '
+                        wrong_answer_templates = ['Not this time :/', 'Yeah no, not really', 'Sorry, try again', 'Good guess, but wrong']
+                    print(f"{random.choice(wrong_answer_templates)}\nLetters tried: {wrong_guessed_letters}" )
+        else:
+            if current_guess == ''.join(word_letters_array):
+                print('OMG SLAY U WIN')
+                break
             else:
                 left_tries -=1 
                 if left_tries == 0:
                     print('Oh noo, looks like ur a lil looser baby. U can always try again doe.')
-                else: 
-                    guessed_letters.remove(current_guess)
-                    wrong_guessed_letters += current_guess + ' '
-                    wrong_answer_templates = ['Not this time :/', 'Yeah no, not really', 'Sorry, try again', 'Good guess, but wrong']
-                    print(f"{random.choice(wrong_answer_templates)}\nLetters tried: {wrong_guessed_letters}" )
+                    break
+                else:
+                    print("That's just completely wrong...")
 
 if __name__ == '__main__':
     word, letters_array = get_word()
